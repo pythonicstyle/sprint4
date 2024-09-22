517866602643544dca3daed48122651fb2c68df5
Garanin Anton <moldycheeese@yandex.ru>
2024-09-22 20:44:19 +0300

    FAQ answers check

--- src/main/java/edu/praktikum/sprint4/rules/BrowserRule.java
+++ src/main/java/edu/praktikum/sprint4/rules/BrowserRule.java
@@ -2,28 +2,26 @@ package edu.praktikum.sprint4.rules;
 
 import static java.time.temporal.ChronoUnit.SECONDS;
 
-import edu.praktikum.sprint4.pom.MainPage;
 import java.time.Duration;
 import org.junit.rules.ExternalResource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class BrowserRule extends ExternalResource {
 
     private WebDriver webDriver;
 
     protected void before() {
-//        String browser = System.getProperty("browser");
-//
-//        if ("chrome".equals(browser)) {
-//            webDriver = new ChromeDriver();
-//        } else if ("firefox".equals(browser)) {
-//            webDriver = new FirefoxDriver();
-//        }
+        String browser = System.getProperty("browser");
 
-        webDriver = new ChromeDriver();
+        if ("firefox".equals(browser)) {
+            webDriver = new FirefoxDriver();
+        } else {
+            webDriver = new ChromeDriver();
+        }
+
         webDriver.manage().timeouts().implicitlyWait(Duration.of(3, SECONDS));
         String url = "https://qa-scooter.praktikum-services.ru/";
         webDriver.get(url);
